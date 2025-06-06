20223250e431659daf90d716cf487b13d9863b0f
Rishitha-star26 <rishitha.23bce7544@vitapstudent.ac.in>
2025-06-06 14:08:28 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,41 @@
+//write an abstract class shape with a method area() . derive circle and rectangle from its override the method . write a main function to compute the area of both .
+abstract class Shape {
+    abstract double area();
+}
+
+class Circle extends Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double area() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    double length, width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    double area() {
+        return length * width;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5.0);
+        Shape rectangle = new Rectangle(4.0, 6.0);
+
+        System.out.println("Area of Circle: " + circle.area());
+        System.out.println("Area of Rectangle: " + rectangle.area());
+    }
+}
